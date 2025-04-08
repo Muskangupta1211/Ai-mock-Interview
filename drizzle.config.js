9baf6e7133940b688d3f0366c1533d68856a4d35
Muskangupta1211 <93373612+Muskangupta1211@users.noreply.github.com>
2025-04-08 20:05:19 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://neondb_owner:npg_Lpoxh4lW0tTU@ep-divine-unit-a55mzb7r-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+        url: '*******your postgresql string*****',
     }
 };
